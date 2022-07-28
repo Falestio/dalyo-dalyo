d7d1ceff397d235c08ef655fca7ec9834b320c3d
Falestio <falestioltmpt@gmail.com>
2022-07-28 19:21:03 +0700

    test state management

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
     <div>
-        <h1 class="text-4xl">This is DaLYO-DaLYO</h1>
+        
     </div>
 </template>
---
+++ pages/test.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { useCounterStore } from '~~/store/counter';
+
+const count = useCounterStore()
+
+const useCount = useState('counter', () => 0)
+
+const cookieCount = useCookie<number>('count')
+
+function add1(){
+    useCount.value++
+}
+
+function saveCookie(){
+    cookieCount.value += 20
+}
+
+</script>
+
+<template>
+    <div>
+        <h1 class="text-4xl">This is DaLYO-DaLYO</h1>
+        <p class="text-2xl">The Pinia Counter is: {{count.count}}</p>
+        <p class="text-2xl">The useState Counter is: {{ useCount }}</p>
+        <p class="text-2xl">The cookieCount is: {{ cookieCount }}</p>
+
+        <button class="btn" @click="count.increment()">Add</button>
+        <button class="btn" @click="add1()">Add useState</button>
+        <button class="btn" @click="saveCookie()">add cookie</button>
+
+    </div>
+</template>
